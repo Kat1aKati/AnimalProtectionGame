85af014d210af693794743261f5a8569a1739f64
Kat1a <hracjednotka@gmail.com>
2025-12-01 01:36:14 +0100

    Update main_game.py
    Update for saving this file

--- main_game.py
+++ main_game.py
@@ -1,6 +1,7 @@
 #taže, prvy level by sme mohli dať ako tutorial. Hybal by si sa WASD (treba pridať) alebo šipky
 #V prvom leveli by mohol player odtlačiť ten pushable box na nejake miesto aby sa mu otvorili dvere a pustili by ho do dalšieho levelu
 #NPC (to ai) by mohlo mať nejaky hitbox aby sa s nim mohlo hybať, možno interagovať
+#commit needed!
 
 import pygame
 import random
